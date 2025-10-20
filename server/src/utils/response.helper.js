fbbcb30d358c674ced874a1f5208bf99a06ab8c1
Surya Teja Kancharla <suryatejakancharla2005@gmail.com>
2025-10-21 02:32:27 +0530

    Fix: correct base path for GitHub Pages

--- server/src/utils/response.helper.js
+++
@@ -1,9 +0,0 @@
-function success(res, data = {}, message = 'OK', code = 200) {
-  return res.status(code).json({ success: true, message, data });
-}
-
-function error(res, message = 'Error', code = 500) {
-  return res.status(code).json({ success: false, message });
-}
-
-module.exports = { success, error };
