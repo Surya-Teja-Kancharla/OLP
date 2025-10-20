caf50071aa1c8095c69cce3bef164b1e19d94373
Surya Teja Kancharla <suryatejakancharla2005@gmail.com>
2025-10-20 12:11:57 +0530

    Modified root initialization code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+// Context Providers
 import { AuthProvider } from "./context/AuthContext";
 import { CourseProvider } from "./context/CourseContext";
 import { ForumProvider } from "./context/ForumContext";
+
+// Global styles
 import "./index.css";
 
+// Mount App
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
